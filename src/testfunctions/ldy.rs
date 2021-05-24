e5c6399f73b34c4144953f1d91b917275cafb7c7
Meqolo <45327591+Meqolo@users.noreply.github.com>
2021-05-24 21:15:40 +0100

    Complete LDY tests

--- src/testfunctions/ldy.rs
+++ src/testfunctions/ldy.rs
@@ -59,54 +59,34 @@ impl LDYTests for Test {
     }
 
     fn absolute() -> () {
-        const EXPECTED_CYCLES: u32 = 4;
         let (mut memory, mut processor) = setup();
 
-        memory.data[0xFFFC] = opcodes::LDA_ABSOLUTE;
-        memory.data[0xFFFD] = 0x80;
-        memory.data[0xFFFE] = 0x44; // 0x4480
-        memory.data[0x4480] = 0x84;
-
-        let cycles = processor.execute(&mut memory, EXPECTED_CYCLES);
-
-        verify_register(&processor, Accumulator, 0x84);
-        verify_cycles(cycles, EXPECTED_CYCLES as i64);
-        verify_lda_flags(&mut processor);
+        test_register_absolute(&mut memory, &mut processor, RegisterX, LDX_ABSOLUTE);
     }
 
     fn absolute_x() -> () {
-        const EXPECTED_CYCLES: u32 = 4;
         let (mut memory, mut processor) = setup();
 
-        processor.register_y = 1;
-
-        memory.data[0xFFFC] = opcodes::LDA_ABSOLUTE_Y;
-        memory.data[0xFFFD] = 0x80;
-        memory.data[0xFFFE] = 0x44; // 0x4480
-        memory.data[0x4481] = 0x84;
-
-        let cycles = processor.execute(&mut memory, EXPECTED_CYCLES);
-
-        verify_register(&processor, Accumulator, 0x84);
-        verify_cycles(cycles, EXPECTED_CYCLES as i64);
-        verify_lda_flags(&mut processor);
+        test_register_absolute_register(
+            &mut memory,
+            &mut processor,
+            RegisterY,
+            LDY_ABSOLUTE_X,
+            Some(RegisterX),
+            false,
+        );
     }
 
     fn absolute_x_overflow() -> () {
-        const EXPECTED_CYCLES: u32 = 5;
         let (mut memory, mut processor) = setup();
 
-        processor.register_y = 0xFF;
-
-        memory.data[0xFFFC] = opcodes::LDA_ABSOLUTE_Y;
-        memory.data[0xFFFD] = 0x02;
-        memory.data[0xFFFE] = 0x44; // 0x4402
-        memory.data[0x4501] = 0x84; // 0x4402 + 0xFF crosses page boundary
-
-        let cycles = processor.execute(&mut memory, EXPECTED_CYCLES);
-
-        verify_register(&processor, Accumulator, 0x84);
-        verify_cycles(cycles, EXPECTED_CYCLES as i64);
-        verify_lda_flags(&mut processor);
+        test_register_absolute_register(
+            &mut memory,
+            &mut processor,
+            RegisterY,
+            LDY_ABSOLUTE_X,
+            Some(RegisterX),
+            true,
+        );
     }
 }
