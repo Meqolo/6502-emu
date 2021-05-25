c404c2927034bc9c94c50a59658b356c0032b3db
Meqolo <45327591+Meqolo@users.noreply.github.com>
2021-05-25 08:38:10 +0100

    Use set_register function rather than directly set value inorder to reduce code

--- src/cpu/instructions/lda.rs
+++ src/cpu/instructions/lda.rs
@@ -14,21 +14,21 @@ impl LoadAccumulator for Processor {
         zero_page_address += self.register_x;
         self.cycles -= 1;
         let effective_address: u16 = self.read_2byte(memory, zero_page_address as u16);
-        self.accumulator = self.read_byte(memory, effective_address);
+        let byte_value = self.read_byte(memory, effective_address);
 
-        self.set_flags_based_on_register(&Accumulator);
+        self.set_register(Accumulator, byte_value)
     }
 
     fn lda_indirect_y(&mut self, memory: &Memory) -> () {
         let zero_page_address: u8 = self.fetch_byte(memory);
         let effective_address: u16 = self.read_2byte(memory, zero_page_address as u16);
         let effective_address_y: u16 = effective_address + self.register_y as u16;
-        self.accumulator = self.read_byte(memory, effective_address_y);
+        let byte_value = self.read_byte(memory, effective_address_y);
 
         if effective_address_y - effective_address >= 0xFF {
             self.cycles -= 1;
         }
 
-        self.set_flags_based_on_register(&Accumulator);
+        self.set_register(Accumulator, byte_value);
     }
 }
--- src/cpu/instructions/loadregisters.rs
+++ src/cpu/instructions/loadregisters.rs
@@ -6,8 +6,7 @@ use crate::cpu::processor::*;
 use crate::{fetch_bit, Memory};
 
 pub trait LoadRegister {
-    // fn set_register_to_use(&mut self, register: Registers, variable: &mut u8) -> ();
-    fn set_flags_based_on_register(&mut self, register: &Registers) -> ();
+    fn set_register(&mut self, register: Registers, value: u8) -> ();
 
     fn load_immediate(&mut self, memory: &Memory, register: Registers) -> ();
     fn load_zero_page(
@@ -26,17 +25,20 @@ pub trait LoadRegister {
 }
 
 impl LoadRegister for Processor {
-    fn set_flags_based_on_register(&mut self, register: &Registers) -> () {
+    fn set_register(&mut self, register: Registers, value: u8) -> () {
         match register {
             Accumulator => {
+                self.accumulator = value;
                 self.set_status(ZeroFlag, self.accumulator == 0);
                 self.set_status(NegativeFlag, fetch_bit(self.accumulator, 7))
             }
             RegisterX => {
+                self.register_x = value;
                 self.set_status(ZeroFlag, self.register_x == 0);
                 self.set_status(NegativeFlag, fetch_bit(self.register_x, 7))
             }
             RegisterY => {
+                self.register_y = value;
                 self.set_status(ZeroFlag, self.register_y == 0);
                 self.set_status(NegativeFlag, fetch_bit(self.register_y, 7))
             }
@@ -44,13 +46,8 @@ impl LoadRegister for Processor {
     }
 
     fn load_immediate(&mut self, memory: &Memory, register: Registers) -> () {
-        match register {
-            Accumulator => self.accumulator = self.fetch_byte(&memory),
-            RegisterX => self.register_x = self.fetch_byte(&memory),
-            RegisterY => self.register_y = self.fetch_byte(&memory),
-        }
-
-        self.set_flags_based_on_register(&register);
+        let byte_value = self.fetch_byte(&memory);
+        self.set_register(register, byte_value);
     }
 
     fn load_zero_page(
@@ -62,13 +59,7 @@ impl LoadRegister for Processor {
         let zero_page_addr: u16 = self.addr_zero_page(&memory, offset_register);
         let byte_value: u8 = self.read_byte(&memory, zero_page_addr);
 
-        match register {
-            Accumulator => self.accumulator = byte_value,
-            RegisterX => self.register_x = byte_value,
-            RegisterY => self.register_y = byte_value,
-        }
-
-        self.set_flags_based_on_register(&register);
+        self.set_register(register, byte_value);
     }
 
     fn load_absolute(
@@ -79,12 +70,7 @@ impl LoadRegister for Processor {
     ) -> () {
         let absolute_addr: u16 = self.addr_absolute(&memory, offset_register);
         let byte_value: u8 = self.read_byte(memory, absolute_addr as u16);
-        match register {
-            Accumulator => self.accumulator = byte_value,
-            RegisterX => self.register_x = byte_value,
-            RegisterY => self.register_y = byte_value,
-        }
 
-        self.set_flags_based_on_register(&register);
+        self.set_register(register, byte_value);
     }
 }
